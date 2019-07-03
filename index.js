83e978ca88b9e0163c8a92a225eb7417abca7886
Amber Meritt <amber.merittcampbell@gmail.com>
2019-07-02 20:16:56 -0700

    added api keys/url

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("js attached");
+
+const apiKeyYelp = "CjrGwySGXlQ7WM-8KxkcW6UmqNdbLrAWSPuVQ94h_oicfw2sAH7kg1QhcRDqJwwxnlYeJt7mHGQRHIxXhV-Nu9uWlwuioOzwEdY_hGELQexdt7TqTB22UMzDnBscXXYx";
+const urlYelp = "https://api.yelp.com/v3/categories/";
+const apiKeyMovieGlu = "0mcK8liAA823jNLp5iqfmV5lAu467j84v4OW3I6c";
+const urlMovieGlu = "	https://api-gate2.movieglu.com/";
