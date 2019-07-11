6efa745db6d94c880610511bf6d1acf56325a06e
Amber Meritt <amber.merittcampbell@gmail.com>
2019-07-11 11:57:02 -0700

    added cinema api

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 console.log("js attached");
 
+let d = new Date()
+
 const apiKeyYelp = "CjrGwySGXlQ7WM-8KxkcW6UmqNdbLrAWSPuVQ94h_oicfw2sAH7kg1QhcRDqJwwxnlYeJt7mHGQRHIxXhV-Nu9uWlwuioOzwEdY_hGELQexdt7TqTB22UMzDnBscXXYx";
 const urlYelp = "https://api.yelp.com/v3/categories/";
 
 const apiKeyMovieGlu = "0mcK8liAA823jNLp5iqfmV5lAu467j84v4OW3I6c";
-const urlMovieGlu = "	https://api-gate2.movieglu.com/";
-const movieHeader = {
-    "client" : "THIN_2",
-    "x-api-key" : apiKeyMovieGlu,
-    "authorization" : "Basic VEhJTl8yOlF3WlJuM0prNks2Tg==",
-    "territory" : "US",
-    "api-version" : "v200",
-    "device-datetime" : ""/*2018-09-14T08:30:17.360Z*/,
-    "geolocation" : `${responseJson.records[0].fields.latitude};${responseJson.records[0].fields.longitude}`
-};
+const urlMovieGlu = "https://api-gate2.movieglu.com/";
 
-function getMoviesByLocation(){
-    
-}
+ function getCinemasByLocation(zipResponseJson){
+    const cinemaHeader = {
+        headers: new Headers({
+            "client" : "THIN_2",
+            "x-api-key" : apiKeyMovieGlu,
+            "authorization" : "Basic VEhJTl8yOlF3WlJuM0prNks2Tg==",
+            "territory" : "US",
+            "api-version" : "v200",
+            "device-datetime" : d.toISOString(),
+            "geolocation" : `${zipResponseJson.records[0].fields.latitude};${zipResponseJson.records[0].fields.longitude}`
+        })
+    };
+    const cinemaUrl = urlMovieGlu + "cinemasNearby/?n=24";
+
+    fetch(cinemaUrl, cinemaHeader).then(cinemaResponse => {
+        if(cinemaResponse.ok){
+            return cinemaResponse.json();
+        }
+        throw new Error(cinemaResponse.statusText);
+    }).then(cinemaResponseJson => console.log(cinemaResponseJson))
+    .catch(err => {
+        $('#js-error-message').text(`Something Failed: ${err.message}`);
+    })
+ }
 
 function getZipCodeLatLong(searchTerm){
     const zipParams = {
@@ -28,12 +42,12 @@ function getZipCodeLatLong(searchTerm){
     let queryString = $.param(zipParams);
     const zipUrl = "https://public.opendatasoft.com/api/records/1.0/search?" + queryString;
     console.log("url", zipUrl);
-    fetch(zipUrl).then(response => {
-        if(response.ok){
-            return response.json();
+    fetch(zipUrl).then(zipResponse => {
+        if(zipResponse.ok){
+            return zipResponse.json();
         }
-        throw new Error(response.statusText);
-    }).then(responseJson => console.log(responseJson))
+        throw new Error(zipResponse.statusText);
+    }).then(zipResponseJson => getCinemasByLocation(zipResponseJson))
     .catch(err => {
         $('#js-error-message').text(`Something Failed: ${err.message}`);
     })
